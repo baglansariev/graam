0b288c156908f181d5e813fff5b4539e442d354a
Baglan Sariev <baglansariev@mail.ru>
2020-07-10 15:53:01 +0600

    font changes 10

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>{{ $title ?? 'GRAAM' }}</title>
         <link rel="shortcut icon" href="{{ asset('images/template/favicon.ico') }}" type="image/x-icon">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-{{--        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">--}}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('font-awesome/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
