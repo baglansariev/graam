6144e18d3d66666b8bf444a7ca2cacb3eb277f00
baglan-s <baglansariev@mail.ru>
2020-09-22 16:24:37 +0600

    js fix 2

--- resources/views/modules/modals/join.blade.php
+++ resources/views/modules/modals/join.blade.php
@@ -11,13 +11,8 @@
                     <div class="popup-content d-flex justify-content-center">
                         <div class="content d-flex flex-column align-items-start">
                             <div class="alert modal-popup-alert" role="alert">
-                                <span class="message d-none"></span>
+                                <span class="message"></span>
                                 <h3>Спасибо!</h3>
-                                <p>
-                                    Ваша заявка на продажу принята,
-                                    <br>
-                                    наш менеджер скоро свяжется с вами
-                                </p>
                             </div>
                         </div>
                     </div>
