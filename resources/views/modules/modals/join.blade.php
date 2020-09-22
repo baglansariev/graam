8a662e736d83fb6c644ce4852d42a5cf8fbd07f0
baglan-s <baglansariev@mail.ru>
2020-09-22 16:34:35 +0600

    js fix 3

--- resources/views/modules/modals/join.blade.php
+++ resources/views/modules/modals/join.blade.php
@@ -5,18 +5,10 @@
             <span></span>
         </div>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="popup-content d-flex justify-content-center">
-                        <div class="content d-flex flex-column align-items-start">
-                            <div class="alert modal-popup-alert" role="alert">
-                                <span class="message"></span>
-                                <h3>Спасибо!</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="container d-block">
+            <div class="row d-flex align-items-center justify-content-center flex-column" style="margin-top: 20%;">
+                <span class="message" style="font-size: 18px;">Спасибо за вашу заявку!</span>
+                <h4>Наш менеджер скоро вам позвонит!</h4>
             </div>
         </div>
 
