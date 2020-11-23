22d84c37296a2426aa47dc0ec4a3b4b9d6042afd
baglan-s <baglansariev@mail.ru>
2020-11-23 19:57:34 +0600

    test logo 5

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="yandex-verification" content="c2e6180560bd2d69" />
-    <title>{{ $title ?? 'GRAAM' }}</title>
+    <title>{{ $title ?? 'ГРААМ' }}</title>
     <link rel="shortcut icon" href="{{ asset('images/template/favicon.ico') }}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
--- resources/views/layouts/personal.blade.php
+++ resources/views/layouts/personal.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title ?? 'GRAAM' }}</title>
+    <title>{{ $title ?? 'ГРААМ' }}</title>
     <link rel="shortcut icon" href="{{ asset('images/template/favicon.ico') }}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
