1bab7b5360409413e7919c95456289b6fed5157d
hawock <hawock@mail.ru>
2020-12-09 16:06:03 +0500

    fix SMTP

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -15,8 +15,8 @@
     <link rel="stylesheet" href="{{ asset('css/feedback-form.css') }}">
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
     <script src="{{ asset('js/jquery.coockie.js') }}"></script>
-    <script src="{{ asset('font-awesome/js/all.min.js') }}"></script>
     <script src="{{ asset('js/jquery.inputmask.min.js') }}"></script>
+    <script src="{{ asset('js/inputmask.min.js') }}"></script>
     <script src="{{ asset('js/inputmask.min.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
     @if (request()->getHost() == 'graam.ru')
